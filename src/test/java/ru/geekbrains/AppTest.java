a4692c00cfcadc5841d50f3e518c2785595c2811
dijinx <dimajinx@gmail.com>
2021-02-25 19:20:49 +0700

    1

---
+++ src/test/java/ru/geekbrains/AppTest.java
@@ -0,0 +1,22 @@
+package ru.geekbrains;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("START TEST");
+        assertTrue( true );
+        System.out.println("END TEST");
+    }
+}
